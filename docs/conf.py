610ac7452490925da05c5b0b5934d3a0f4f7a16a
vesalkasmaeifar <vesalkasmaei1999@gmail.com>
2025-10-31 12:28:37 -0400

    RTD

--- docs/conf.py
+++ docs/conf.py
@@ -2,16 +2,57 @@
 project = "whisper"
 author = "Vesal Kasmaeifar"
 
-# -- General configuration ---------------------------------------------------
 extensions = [
-    "myst_parser",  # allow Markdown support
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'myst_nb',
+    'sphinx_rtd_theme',
 ]
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# -- Options for HTML output -------------------------------------------------
-html_theme = "sphinx_rtd_theme"
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 
-# MyST (Markdown) options
-myst_enable_extensions = ["linkify"]
+# Napoleon settings for Google/NumPy style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+
+# MyST settings
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "html_image",
+]
+
+# MyST-NB settings for notebook execution
+nb_execution_mode = "off"  # Don't execute notebooks during build
+nb_execution_timeout = 60
+myst_nb_render_priority = {
+    "html": ("application/vnd.jupyter.widget-view+json", "text/html", "image/png", "text/plain"),
+    "latex": ("text/latex", "image/png", "text/plain")
+}
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'sklearn': ('https://scikit-learn.org/stable/', None),
+    'opencv': ('https://docs.opencv.org/4.x/', None),
+    'optuna': ('https://optuna.readthedocs.io/en/stable/', None),
+}
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
+
+autosummary_generate = True 
