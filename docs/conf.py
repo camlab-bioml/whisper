470382ed4a26290eeb1509725fc5e29049c5e9a2
vesalkasmaeifar <vesalkasmaei1999@gmail.com>
2026-09-22 18:16:26 -0400

    whisper v2

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 
 # Napoleon settings for Google/NumPy style docstrings
 napoleon_google_docstring = True
